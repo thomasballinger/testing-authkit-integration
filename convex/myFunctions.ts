d753c723eb49d0953609865055d3f7d2e57af46e
Tom Ballinger <tom@convex.dev>
2026-01-09 13:40:59 -0800

    test 2

--- convex/myFunctions.ts
+++ convex/myFunctions.ts
@@ -2,6 +2,8 @@ import { v } from 'convex/values';
 import { query, mutation, action } from './_generated/server';
 import { api } from './_generated/api';
 
+// change
+
 // Write your Convex functions in any file inside this directory (`convex`).
 // See https://docs.convex.dev/functions for more.
 
