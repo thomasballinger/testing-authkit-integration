cfcd97ff9fb9d7d29fba7b77f1dcf7bf7873f9fd
Tom Ballinger <tom@convex.dev>
2026-01-09 15:25:22 -0800

    testing

--- app/credential.ts
+++ app/credential.ts
@@ -1,7 +1,11 @@
 // Save the bundled-in key to process.env
 console.log('saving WORKOS_API_KEY');
-process.env.WORKOS_API_KEY = process.env.WORKOS_API_KEY;
 const foo = process;
 const bar = foo.env;
 const baz = bar.WORKOS_API_KEY;
 console.log(!!baz);
+process.env.WORKOS_API_KEY = process.env.WORKOS_API_KEY;
+const foo2 = process;
+const bar2 = foo2.env;
+const baz2 = bar2.WORKOS_API_KEY;
+console.log(!!baz2);
